557b670ba526e7a2dbe58895ac1dfca784c0ecbc
Yuta Takahashi <u.ta4263@gmail.com>
2018-06-05 07:15:07 +0000

    Procfile

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -6,7 +6,7 @@
 
     <h1>タスク新規作成ページ</h1>
 
-  <div class="row">
+  　　　<div class="row">
          <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
             
